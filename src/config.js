ba6a8efc43de83c48768b0929a49236a9bb08734
Cindy Zhang <cynthiacheung831@gmail.com>
2020-12-16 16:21:46 -0600

    minor edit in model

--- src/config.js
+++ src/config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  // db: {
-  //   database: process.env.DB_NAME || 'RecipeApp',
-  //   user: process.env.DB_USER || 'root',
-  //   password: process.env.DB_PASS || 'zxy19960831',
-  //   options: {
-  //     dialect: process.env.DIALECT || 'mysql',
-  //     host: process.env.HOST || 'localhost',
-  //     port: process.env.DB_PORT || '3306'
-  //   }
-  // },
   db: {
     database: process.env.DB_NAME || 'heroku_cd3d6d1c747a95c',
     user: process.env.DB_USER || 'b1703ff315c6e9',
